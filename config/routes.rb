4f969e76e0c6fcc4c42f30e24efeec1fc3de45dc
Seok Jong Yu <seokjongyu@gmail.com>
2018-01-08 16:50:47 +0900

    add a FASTA data into user's project #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+
   get 'totalanalysis' => 'static#total_analysis', as: :totalanalysis
 
-  resources :projects
+  resources :projects do
+    resources :data
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "static#index"
