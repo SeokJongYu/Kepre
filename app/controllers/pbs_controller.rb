fe895081e3670dffad0072ccd8800b475271889a
Seok Jong Yu <seokjongyu@gmail.com>
2018-01-10 23:39:07 +0900

    submit of analysis job

--- app/controllers/pbs_controller.rb
+++ app/controllers/pbs_controller.rb
@@ -2,25 +2,14 @@ class PbsController < ApplicationController
     before_action :get_batch_server
 
     def jobs
-        @b.get_status
+        #@job_id = @b.submit_script("/home/codegen/Dev/Kepre/public/test.sh")
+        @stat = @b.get_job("5.manjaro-codegen")
     end
 
     def stat 
         @b.get_status
     end
 
-    def qsub
-        @job_id = b.submit_script("/path/to/script")
-
-    end
-
-    def qdel
-        @b.delete_job(job_id)
-    end
-
-    def get_job_info
-        2b.get_job(job_id)
-    end
 
     private
     # Create torque batch object for job handling
