ee4cdd16cea12f799854e05ed2e28297f51077d7
Seok Jong Yu <seokjongyu@gmail.com>
2018-01-14 16:31:57 +0900

    adding RSpec and Capybara, Guard for BDD about issue #13

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,8 @@
 class Project < ApplicationRecord
     extend FriendlyId
     friendly_id :title, use: :slugged
-
+    validates :title, presence: true
+    validates :title, length: {maximum: 100}
     has_many :data, dependent: :destroy
     
     def should_generate_new_friendly_id?
