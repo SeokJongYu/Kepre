dd4e59ef70ae8ef8b503f56a1c23a3fbf099343a
Seok Jong Yu <seokjongyu@gmail.com>
2018-02-18 11:49:21 +0900

    adding the MHC II analysis form #52

--- app/models/mhcii_item.rb
+++ app/models/mhcii_item.rb
@@ -2,4 +2,26 @@ class MhciiItem < ApplicationRecord
   TYPE = "MHC-II"
   belongs_to :datum
   has_one :tool_item, as: :itemable
+  has_one :analysis, through: :tool_item
+
+  def getType
+    TYPE
+  end
+
+  def getArgs
+    "#{prediction_method} \"#{alleles}\" "
+  end
+
+  def getMethod
+    prediction_method
+  end
+
+  def getAlleles
+    alleles
+  end
+
+  def getOutputSort
+    output_sort
+  end
+
 end
